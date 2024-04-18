a0844de560a5e9ca4f93518881c316c772e45e5f
aulianita31 <aulianita173@gmail.com>
2024-04-18 20:56:02 +0700

    Belajar Git TKJ30

---
+++ home.php
@@ -0,0 +1,16 @@
+<div class="card">
+    <div class="card-body">
+        <div>
+            <img src="images/tkj-logo-tkj-letter-tkj-letter-logo-design-initials-tkj-logo-linked-with-circle-and-uppercase-monogram-logo-tkj-typography-for-technology-business-and-real-estate-brand-vector.jpg" alt="TKJ" class="float-end" height="200px"><br>
+            Kelas TKJ merupakan kelasnya para programer bagi anda yang mau menjadi seorang progremer yang handal,  dikelas ini kamu akan di ajarkan skill seputar dunia Komputer dan Jaringannya mulai dari belajar mikrotik, server, dan membuat Web Development.
+            <ol>
+                <li>Mikrotik membahas tentang bagaimana menghubungkan komputer ke internet</li>
+                <li>Server membahas bagaimana menjalankan server menggunakan CentOS7</li>
+                <li>Web development membahas bagaimana membuat dan merancang sebuah web</li>
+                <li>Hardware & software membahas bagaimana merakit sebuah PC serta menginstal aplikasi</li>
+                <li>Menulis kreatif</li>
+            </ol>
+            <p>Lulusan teknik komputer dan jaringan menghasilkan paraprogrammer handal yang nantinya akan bergelut didunia IT alumni lulusan TKJ banyak yang bekerja di perusahaan IT dan beberapa lembaga yang membutuhkan keahlian mengenai IT.</p>
+        </div>
+    </div>
+</div>
